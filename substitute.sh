a45c8793d97541f0e048d081af8c86aca75dde3e
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2022-02-02 11:49:55 +0100

    Allow to specify public/private

--- substitute.sh
+++ substitute.sh
@@ -2,5 +2,7 @@
 # Replace template strings such as THISHOST, for host independence
 for i in "$@"
 do
-  sed -e "s/THISHOST/${THISHOST:-this host}/" < "$i" > "${i/.IN/}"
+  sed -e "s/__THISHOST__/${THISHOST:-this host}/" \
+      -e "s/__ACCESS__/${ACCESS:-Public}/" \
+    < "$i" > "${i/.IN/}"
 done
