cb325e0528f6a896cbb6f4b25164fa2577c51881
Mike Cifelli <1836280-mike-cifelli@users.noreply.gitlab.com>
2022-11-26 12:44:25 -0500

    Show stratum and reference on home page

--- substitute.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Replace template strings such as THISHOST, for host independence
-for i in "$@"
-do
-  sed -e "s/__THISHOST__/${THISHOST:-this host}/" \
-      -e "s/__ACCESS__/${ACCESS:-Public}/" \
-    < "$i" > "${i/.IN/}"
-done
